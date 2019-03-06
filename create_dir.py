2c8f202f46987a8721d05c19150c217d29b451cf
Sunbaoli <41151640+Sunbaoli@users.noreply.github.com>
2019-03-06 22:08:30 +0800

    Create create_dir.py

---
+++ create_dir.py
@@ -0,0 +1,8 @@
+import os
+def mkdir(paths):
+    if not isinstance(paths, (list, tuple)):
+        paths = [paths]
+    for path in paths:
+        if not os.path.isdir(path):
+            os.makedirs(path)
+mkdir('./color_train_HR_x8_256')
